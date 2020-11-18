59965fce4fe7eddf960cc6fb1c2d9c428d4532d6
Antonio Ruby Barreto <tomrubybarreto@gmail.com>
2020-11-19 00:51:38 +0100

    Remove naive extract_mock_functions

--- git/test_git_hooks.sh
+++ git/test_git_hooks.sh
@@ -10,18 +10,17 @@ oneTimeSetUp() {
   OUTFILE="${SHUNIT_TMPDIR:?}/prepare_msg"
 }
 
-# Prepare mocks and call prepare-commit-msg script
-# Args:
-#   $1: initial contents of commit msg
-prepare_msg() {
-  eval "$(extract_mock_functions)" || exit 2
+tearDown() {
+  cleanupSpies
+  cleanupTestDir
+}
 
+# Call prepare-commit-msg hook in tmp file
+# $1: initial contents of commit msg
+prepare_msg() {
   rm -f "$OUTFILE"
   echo "$1" > "$OUTFILE"
-
-  # Sourcing, instead of calling, to use mocked git functions in tests.
-  # shellcheck source=templates/hooks/prepare-commit-msg
-  (msgfile="$OUTFILE" . "$THISDIR/templates/hooks/prepare-commit-msg")
+  "$THISDIR/templates/hooks/prepare-commit-msg" "$OUTFILE"
 }
 
 #
@@ -29,47 +28,73 @@ prepare_msg() {
 #
 
 test_tag_is_inserted_at_message_start() {
-  mock_git() { echo 'ABC-123'; }
-  prepare_msg ''
+  createSpy -o "ABC-123-my-feature" git
+  prepare_msg ""
   assertEquals "[ABC-123] " "$(cat "$OUTFILE")"
 }
 
 test_tag_is_inserted_preserving_previous_message() {
-  mock_git() { echo 'ABC-123'; }
-  prepare_msg 'msg'
+  createSpy -o "ABC-123_my_feature" git
+  prepare_msg "msg"
   assertEquals "[ABC-123] msg" "$(cat "$OUTFILE")"
 }
 
 test_tag_is_not_inserted_if_its_not_an_issue_like_format() {
-  mock_git() { echo 'HEAD^1'; }
-  prepare_msg 'msg'
+  createSpy -o "HEAD^1" git
+  prepare_msg "msg"
   assertEquals "msg" "$(cat "$OUTFILE")"
 
-  mock_git() { echo 'abc-123'; } # lowercase is not tag
-  prepare_msg 'msg'
+  createSpy -o "abc-123" git # lowercase is not tag
+  prepare_msg "msg"
   assertEquals "msg" "$(cat "$OUTFILE")"
-}
-
-test_branch_prefix_is_removed_to_infer_tag_name() {
-  mock_git() { echo 'feature/AB-42-my-feat'; }
-  prepare_msg 'msg'
-  assertEquals "[AB-42] msg" "$(cat "$OUTFILE")"
-
-  mock_git() { echo 'origin/feature/CD-84-my-feat'; }
-  prepare_msg 'msg'
-  assertEquals "[CD-84] msg" "$(cat "$OUTFILE")"
 }
 
 test_tag_is_not_re_inserted_if_message_already_has_tag() {
-  mock_git() { echo 'ABC-123'; }
-  prepare_msg '[DEF-456] msg'
+  createSpy -o "ABC-123" git
+  prepare_msg "[DEF-456] msg"
   assertEquals "[DEF-456] msg" "$(cat "$OUTFILE")"
 
-  mock_git() { echo 'ABC-123'; }
-  prepare_msg '[FIX] msg'
+  createSpy -o "ABC-123" git
+  prepare_msg "[FIX] msg"
   assertEquals "[FIX] msg" "$(cat "$OUTFILE")"
 }
 
+test_tag_is_not_inserted_if_message_is_some_automatic_git_message() {
+  createSpy -o "ABC-123" git
+  prepare_msg "Merge branch ..."
+  assertEquals "Merge branch ..." "$(cat "$OUTFILE")"
+
+  createSpy -o "ABC-123" git
+  prepare_msg "fixup [FIX] msg"
+  assertEquals "fixup [FIX] msg" "$(cat "$OUTFILE")"
+}
+
+test_branch_prefix_is_removed_to_infer_tag_name() {
+  createSpy -o "feature/AB-42-my-feat" git
+  prepare_msg "msg"
+  assertEquals "[AB-42] msg" "$(cat "$OUTFILE")"
+
+  createSpy -o "origin/fEaTurE/CD-84-my-feat" git
+  prepare_msg "msg"
+  assertEquals "[CD-84] msg" "$(cat "$OUTFILE")"
+}
+
+test_it_skips_commented_lines_when_inserting_tag_name() {
+  createSpy -o "ABC-123" git
+  prepare_msg "$(cat <<-EOF
+	# Some comment git added
+	msg
+	EOF
+  )"
+  assertContains "$(cat "$OUTFILE")" "[ABC-123] msg"
+}
+
 # Run tests
+SHPY_PATH="$THISDIR/../tests/shpy"
+export SHPY_PATH
+# shellcheck source=../tests/shpy
+. "$THISDIR/../tests/shpy"
+# shellcheck source=../tests/shpy-shunit2
+. "$THISDIR/../tests/shpy-shunit2"
 # shellcheck source=../tests/shunit2
 . "$THISDIR/../tests/shunit2"
